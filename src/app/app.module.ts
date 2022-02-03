97fabef46822545e314a8adb60d5e576a129e8b0
Bruno Perry <brunoperry@Brunos-Mac-Pro.lan>
2022-02-03 21:02:05 +0000

    initial v2 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,106 +1,90 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { SiteListComponent } from './site-list/site-list.component';
-import { SiteCardComponent } from './components/site-card/site-card.component';
-import { RateComponent } from './components/rate/rate.component';
-import { MenuComponent } from './components/menu/menu.component';
+
 import { RouterModule, Routes } from '@angular/router';
-import { SiteDetailComponent } from './site-detail/site-detail.component';
-import { ReviewCardComponent } from './components/review-card/review-card.component';
-import { SiteMapComponent } from './components/site-map/site-map.component';
+
 import { ReactiveFormsModule } from '@angular/forms';
-import { ProfileComponent } from './profile/profile.component';
-import { SiteMiniCardComponent } from './components/site-mini-card/site-mini-card.component';
-import { ReviewAddComponent } from './review-add/review-add.component';
-import { SettingsComponent } from './settings/settings.component';
-import { CheckboxComponent } from './components/checkbox/checkbox.component';
-import { AboutComponent } from './about/about.component';
-import { LogoutComponent } from './logout/logout.component';
-import { LangButtonComponent } from './components/lang-button/lang-button.component';
-import { SiteEditComponent } from './site-edit/site-edit.component';
 
 //firebase
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore, connectFirestoreEmulator, enableIndexedDbPersistence } from '@angular/fire/firestore';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { environment } from 'src/environments/environment';
-import { GalleryComponent } from './components/gallery/gallery.component';
+import { HomeComponent } from './views/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './views/profile/profile.component';
+import { SearchComponent } from './views/search/search.component';
+import { AddComponent } from './views/add/add.component';
 
-const routes:Routes = [
+const routes: Routes = [
   {
     path: '',
-    redirectTo: '/site-list',
-    pathMatch: 'full'
+    redirectTo: '/home',
+    pathMatch: 'full',
   },
   {
-    path: 'site-list',
-    component: SiteListComponent
+    path: 'home',
+    component: HomeComponent,
   },
   {
-    path: 'site-detail/:index',
-    component: SiteDetailComponent
+    path: 'search',
+    component: SearchComponent,
   },
   {
-    path: 'site-edit/:index',
-    component: SiteEditComponent
-  },
-  {
-    path: 'site-edit',
-    component: SiteEditComponent
+    path: 'add',
+    component: AddComponent,
   },
   {
     path: 'profile',
-    component: ProfileComponent
+    component: ProfileComponent,
   },
-  {
-    path: 'profile/:index',
-    component: ProfileComponent
-  },
-  {
-    path: 'review-add/:index',
-    component: ReviewAddComponent
-  },
-  {
-    path: 'settings',
-    component: SettingsComponent
-  },
-  {
-    path: 'about',
-    component: AboutComponent
-  },
-  {
-    path: 'logout',
-    component: LogoutComponent
-  }
+  // {
+  //   path: 'site-edit/:index',
+  //   component: SiteEditComponent,
+  // },
+  // {
+  //   path: 'site-edit',
+  //   component: SiteEditComponent,
+  // },
+  // {
+  //   path: 'profile',
+  //   component: ProfileComponent,
+  // },
+  // {
+  //   path: 'profile/:index',
+  //   component: ProfileComponent,
+  // },
+  // {
+  //   path: 'review-add/:index',
+  //   component: ReviewAddComponent,
+  // },
+  // {
+  //   path: 'settings',
+  //   component: SettingsComponent,
+  // },
+  // {
+  //   path: 'about',
+  //   component: AboutComponent,
+  // },
+  // {
+  //   path: 'logout',
+  //   component: LogoutComponent,
+  // },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SiteListComponent,
-    SiteCardComponent,
-    RateComponent,
-    MenuComponent,
-    SiteDetailComponent,
-    ReviewCardComponent,
-    SiteMapComponent,
+    HomeComponent,
+    NavbarComponent,
     ProfileComponent,
-    ReviewAddComponent,
-    SettingsComponent,
-    CheckboxComponent,
-    AboutComponent,
-    LogoutComponent,
-    LangButtonComponent,
-    SiteEditComponent,
-    SiteMiniCardComponent,
-    GalleryComponent
+    SearchComponent,
+    AddComponent,
   ],
   imports: [
     BrowserModule,
@@ -110,13 +94,12 @@ const routes:Routes = [
     HttpClientModule,
     ReactiveFormsModule,
     [
-        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-        provideFirestore(() => getFirestore()),
-        provideStorage(() => getStorage())
-    ]
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+      provideFirestore(() => getFirestore()),
+      provideStorage(() => getStorage()),
+    ],
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { 
-}
+export class AppModule {}
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Location } from '@angular/common';
+import { filter } from 'rxjs';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent implements OnInit {
+  public currentView: string = 'home';
+  constructor(location: Location, router: Router) {
+    router.events
+      .pipe(filter((event: any) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentView = location.path().replace('/', '');
+        console.log(this.currentView);
+      });
+  }
+
+  ngOnInit(): void {
+    // this.currentView = location.path().replace('/', '');
+    // console.log(val);
+  }
+
+  onButtonClick(type: string): void {
+    this.currentView = type;
+    switch (type) {
+      case 'home':
+        break;
+      case 'search':
+        break;
+      case 'add':
+        break;
+      case 'profile':
+        break;
+    }
+  }
+}
